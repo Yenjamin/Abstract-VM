94c656d537edce7292eb491961836bde82557a02
yenjamin <94yenho@gmail.com>
2018-07-09 15:46:54 +0200

    a bit more

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
 NAME = avm
 
+SRC = main.cpp \
+
 FLAGS = -Wall -Werror -Wextra
+
+INCLUDE = *.hpp
+
+all: $(NAME)
+
+$(NAME):
+	clang++ -o $(NAME) $(SRC)
+
+clean:
+	rm -f $(NAME)
+
+fclean: clean
+
+re: fclean all
--- main.cpp
+++ main.cpp
@@ -1,3 +1,32 @@
 int	main(void)
 {
+	UserEntry entry;
+
+	try
+	{
+		entry.checkErrorFlag(argc, argv);
+		if (argc == 1)
+			entry.readEntry();
+		else if (argc >= 2)
+			entry.readFile(argv[1]);
+		else {
+			throw UserEntry::WrongArgumentNumber();
+		}
+
+		try
+		{
+			Parser::ParseEntry(entry.getContent());
+		}
+		catch (std::logic_error &e)
+		{
+			std::cout << "Logic error occured: " << e.what() << std::endl;
+			if (entry.getFlagError() == true)
+				TestError(entry);
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error on start: " << e.what() << std::endl;
+	}
+	return (0);
 }
